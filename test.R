340e377738ed116267a1bb8fb2e5c1f08be3e71c
Lawrence Richer <lricher@ualberta.ca>
2022-11-22 23:04:16 -0700

    Update test.R

--- test.R
+++ test.R
@@ -1,1 +1,8 @@
-# My R Script
+# Function to put quotes around everything in list
+.q <- function(...) {
+  s <- sys.call()[-1]
+  w <- as.character(s)
+  n <- names(s)
+  if(length(n)) names(w) <- n
+  w
+}
